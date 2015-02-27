3838695de29697898c61ea9871256276640a3de8
Stuart Clark <stu@rtclark.net>
2015-02-27 14:43:49 +1100

    Fixed tempaltes/server issue

--- src/garagepiface/server/__init__.py
+++ src/garagepiface/server/__init__.py
@@ -3,7 +3,7 @@
 from flask import Flask, render_template
 from flask.ext.bootstrap import Bootstrap
 
-app = Flask('garagepi')
+app = Flask('garagepiface')
 Bootstrap(app)
 
 @app.route('/')
@@ -12,7 +12,7 @@ def index():
 
 class Server():
 	def run(self):
-		app.run()
+		app.run(host='0.0.0.0', port=80)
 
 if __name__ == "__main__":
 	garagepiface = Server()
